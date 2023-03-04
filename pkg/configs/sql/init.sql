7d8cccefb50cf45ed3ffb70d28ac0e97a1bb0b31
root <root@DESKTOP-P3GV7C6>
2023-03-04 15:01:20 +0800

    fixed idl

--- pkg/configs/sql/init.sql
+++ pkg/configs/sql/init.sql
@@ -1,3 +1,4 @@
+-- 用户信息表
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `username`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Username',
@@ -19,7 +20,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY          `idx_username` (`username`) COMMENT 'Username index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='User account table';
-
+-- 视频表
 CREATE TABLE `video` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `author_id` bigint NOT NULL,
@@ -28,6 +29,7 @@ CREATE TABLE `video` (
   `cover_url` varchar(128) NOT NULL,
   `favorite_count` bigint DEFAULT 0,
   `comment_count` bigint DEFAULT 0,
+  `is_favorite`      boolean  NOT NULL DEFAULT 0 COMMENT 'IsFavorite',
   `title` varchar(20) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'video create time',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'video update time',
@@ -35,7 +37,7 @@ CREATE TABLE `video` (
   PRIMARY KEY (`id`),
   KEY          `idx_video_of_user_id` (`author_id`) COMMENT 'VideoOfUserId index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Video table';
-
+-- 用户聊天表
 CREATE TABLE `message`
 (
     `id`         bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
@@ -50,7 +52,7 @@ CREATE TABLE `message`
     KEY          `idx_user_id` (`from_user_id`) COMMENT 'UserId index'
     
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Message table';
-
+-- 用户关系表
 CREATE TABLE `relation` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `from_user_id` bigint NOT NULL,
@@ -63,6 +65,7 @@ CREATE TABLE `relation` (
   KEY          `idx_from_user_id` (`from_user_id`) COMMENT 'Relation index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Relation table';
 
+-- 点赞表
 CREATE TABLE `favorite` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `user_id` bigint NOT NULL,
@@ -74,6 +77,24 @@ CREATE TABLE `favorite` (
   KEY          `idx_from_user_id` (`user_id`) COMMENT 'Favorite index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Favorite table';
 
+-- 评论表
+
+CREATE TABLE `comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `user_id` bigint NOT NULL,
+  `video_id` bigint NOT NULL,
+  `parent_id` bigint NOT NULL,
+	`is_valid`   boolean  NOT NULL DEFAULT 1 COMMENT 'IsValid',
+  `content`    TEXT NULL COMMENT 'Content',
+  `create_date` varchar(128) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'comment create time',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'comment update time',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'comment delete time',
+  PRIMARY KEY (`id`),
+  KEY          `idx_comment_user_id` (`user_id`) COMMENT 'Comment index'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Comment table';
+
+
 ALTER TABLE `video` ADD FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
 ALTER TABLE `message` ADD FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`);
 ALTER TABLE `message` ADD FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`);
@@ -81,3 +102,5 @@ ALTER TABLE `relation` ADD FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`)
 ALTER TABLE `relation` ADD FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`);
 ALTER TABLE `favorite` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 ALTER TABLE `favorite` ADD FOREIGN KEY (`video_id`) REFERENCES `video` (`id`);
+ALTER TABLE `comment` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `comment` ADD FOREIGN KEY (`video_id`) REFERENCES `video` (`id`);
